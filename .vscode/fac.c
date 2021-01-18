c2a6d8c64fb87d277cfe776cdd63e186d0987d24
kumoato <arttyexo201@gmail.com>
2021-01-19 01:58:20 +0700

    first commit

---
+++ .vscode/fac.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main(){
+    system("cls");
+    int f,x,f_tmp;
+
+    printf("Enter factorial number here : ");
+    scanf("%d",&f);
+
+    f_tmp = f;
+    x =1;
+    while (f_tmp>0){
+        
+        x = x * f_tmp;
+        f_tmp--;
+    }
+    printf("Factorial of the num(%d) = %d", f,x);
+    
+}
